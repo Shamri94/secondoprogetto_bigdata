78f56a2ac88dbaf63cc63fc39ec6d09fadea1147
Vincenzo <vmartello.95@gmail.com>
2019-07-19 17:12:51 +0200

    aggiunto salvataggio su relational db

--- saveDataset.sh
+++ saveDataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo docker cp ./data/nodi.json mongodb:/nodi.json
-sudo docker cp ./datas/tazione.json mongodb:/stazione.json
+sudo docker cp ./data/stazione.json mongodb:/stazione.json
 sudo docker exec -it mongodb mongoimport --file nodi.json --db pantheon --collection nodes --jsonArray --username root --password mongodb --authenticationDatabase admin
 sudo docker exec -it mongodb mongoimport --file stazione.json --db pantheon --collection station --jsonArray --username root --password mongodb --authenticationDatabase admin
