5eb79182b084cbbf4ad99c28d70207c7b96a95a5
Vincenzo <vmartello.95@gmail.com>
2019-07-22 10:58:32 +0200

    rimossi file inutili e typo

--- export_data_tomongodb.sh
+++
@@ -1,3 +0,0 @@
-python3 csv_to_json_convert.py
-mongoimport --db pantheon --collection nodes --file data/nodi.json --jsonArray
-mongoimport --db pantheon --colection station --file data/stazione.json --jsonArray
